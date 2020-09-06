b7cb5bd6f73efa09aa1d67882bb3974fa242dc9b
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-09-06 16:29:31 +0300

    Initial Commit

---
+++ FloatingButtonWithAnimatedMenu/FloatingButtonWithAnimatedMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingButtonWithAnimatedMenuApp.swift
+//  FloatingButtonWithAnimatedMenu
+//
+//  Created by Ramill Ibragimov on 06.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct FloatingButtonWithAnimatedMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
